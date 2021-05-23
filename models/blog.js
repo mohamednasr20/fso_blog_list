6e127982de2d959db8aa3773ae136ebffa460be4
mohamednasr20 <mohamednasr8687@gmail.com>
2021-05-23 15:10:56 -0400

    populate users and blogs

--- models/blog.js
+++ models/blog.js
@@ -5,6 +5,10 @@ const blogSchema = new mongoose.Schema({
   author: String,
   url: String,
   likes: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
 });
 
 blogSchema.set('toJSON', {
@@ -12,11 +16,6 @@ blogSchema.set('toJSON', {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
     delete returnedObject.__v;
-  },
-
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
   },
 });
 
